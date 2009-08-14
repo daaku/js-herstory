2cd5b6c125ae8438fe3ed323f7b3cc285e102a9f
Naitik Shah <n@daaku.org>
2009-08-14 03:25:19 +0000

    added tests for Herstory.load - 96% coverage

--- tests.js
+++ tests.js
@@ -24,48 +24,69 @@ var TestData = [
     message: 'Click here and expect "three".'
   },
   {
+    load: 'xyz',
+    expect: 'xyz'
+  },
+  {
+    expect: 'three',
+    message: 'Click the back button and expect "three".'
+  },
+  {
+    expect: 'xyz',
+    message: 'Click the forward button and expect "xyz".'
+  },
+  {
     url: '#',
     expect: '',
     message: 'Click here and expect "".'
   }
 ];
 
-test('human', function() {
+test(
+  'human',
 
-       expect(TestData.length);
+  function() {
 
-       var guide = document.getElementById('guide');
-       var stepIndex = 0;
+    expect(TestData.length);
 
-       function render() {
-         var step = TestData[stepIndex];
-         var html;
-         if (step.url) {
-           html = '<a href="' + step.url + '">' + step.message + '</a>';
-         } else {
-           html = step.message;
-         }
-         guide.innerHTML = html;
-       }
+    var guide = document.getElementById('guide');
+    var stepIndex = 0;
 
-       function hashChange(hash) {
-         var step = TestData[stepIndex];
-         equals(step.expect,
-                hash,
-                "expecting '" + step.expect + "', " +
-                "got '" + hash + "', " +
-                "for step: " + stepIndex);
-         stepIndex++;
+    function doStep() {
+      var step = TestData[stepIndex];
 
-         // either we're done, or we render the next step
-         if (stepIndex == TestData.length) {
-           start();
-           guide.innerHTML = '';
-         } else {
-           render();
-         }
-       }
-	     Herstory.init(hashChange, "herstory_iframe.html");
+      if (step.load) {
+        Herstory.load(step.load);
+        return;
+      }
+      var html;
+      if (step.url) {
+        html = '<a href="' + step.url + '">' + step.message + '</a>';
+      } else {
+        html = step.message;
+      }
+      guide.innerHTML = html;
+    }
 
-       stop();
-     });
+    function hashChange(hash) {
+      var step = TestData[stepIndex];
+      equals(step.expect,
+             hash,
+             "expecting '" + step.expect + "', " +
+             "got '" + hash + "', " +
+             "for step: " + stepIndex);
+      stepIndex++;
+
+      // either we're done, or we doStep the next step
+      if (stepIndex == TestData.length) {
+        start();
+        guide.innerHTML = '';
+      } else {
+        doStep();
+      }
+    }
+    Herstory.init(hashChange, "herstory_iframe.html");
+
+    stop();
+  }
+);
